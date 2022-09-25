2c1d41189f92408051a0c9e471f1265f06665677
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-09-25 11:16:46 -0300

    form: salvando dados (post) + tratando erros

--- src/app/videos/services/videos.service.ts
+++ src/app/videos/services/videos.service.ts
@@ -1,7 +1,7 @@
+import { Video } from './../model/video';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Video } from '../model/video';
 import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -17,8 +17,13 @@ export class VideosService {
     return this.HttpClient.get<Video[]>(this.API)
     .pipe(
       first(), // recebe apenas a primeira resposta do servidor e finaliza a comunicação
-      delay(1000),
+      // delay(1000),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Video) {
+    return this.HttpClient.post<Video>(this.API, record)
+      .pipe(first());
   }
 }
--- src/app/videos/video-form/video-form.component.ts
+++ src/app/videos/video-form/video-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { VideosService } from '../services/videos.service';
 
 @Component({
   selector: 'app-video-form',
@@ -10,7 +13,9 @@ export class VideoFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: VideosService,
+    private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -21,10 +26,15 @@ export class VideoFormComponent implements OnInit {
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value)
+      .subscribe(result => console.log(result), error => this.onError());
   }
 
   onBack() {
 
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar vídeo', '', /*{ duration: 10000 }*/);
   }
 }
